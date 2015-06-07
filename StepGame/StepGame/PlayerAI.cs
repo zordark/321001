6dbc8a2c3cb72595226b524257065e02e8ff27a0
MrEqu <emirtb@rambler.ru>
2015-06-08 03:54:06 +0500

    vdvsd

---
+++ StepGame/StepGame/PlayerAI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StepGame
+{
+    
+    class PlayerAI:Player
+    {
+
+        
+        public PlayerAI(PictureBox picture,
+                     string name, string weaponName):
+            base(picture, name, weaponName)
+        { 
+        }
+
+        
+
+        public override void Move()
+        {
+            PlayerUser user = getUserInRadius(X,Y,Weapon_.Radius);
+
+            if (user != null)
+            {
+                NextMoveType = MoveType.Shot;
+                user.Killed = true;
+            }
+            else
+            {
+                do
+                {
+                    NextDirectionX = r.Next(-10, 10);
+                    NextDirectionX = NextDirectionX != 0 ? NextDirectionX / Math.Abs(NextDirectionX) :0;
+                }
+                while ((X + NextDirectionX) < 0 || (X + NextDirectionX) > 20);
+
+                do
+                {
+                    NextDirectionY = r.Next(-10, 10);
+                    NextDirectionY = NextDirectionY != 0 ? NextDirectionY / Math.Abs(NextDirectionY) : 0;
+                }
+                while ((Y + NextDirectionY) < 0 || (Y + NextDirectionY) > 20);
+
+
+
+                NextMoveType = MoveType.Move;
+
+                
+            }
+            base.Move();
+        }
+    }
+}
---
+++ StepGame/StepGame/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StepGame
+{
+    public class Weapon
+    {
+        public int Radius { get; set; }
+        public string Name { get; set; }
+
+        private Weapon(int radius, string name)
+        {
+            Radius = radius;
+            Name = name;
+        }
+
+        public static Dictionary<string, Weapon> weapons;
+        public static List<Weapon> values;
+        public static List<string> keys;
+        public static Random r;
+        
+        static Weapon()
+        {
+            weapons = new Dictionary<string, Weapon>();
+            weapons["morkov"] = new Weapon(1, "morkov");
+            weapons["serp"] = new Weapon(2, "serp");
+            weapons["molot"] = new Weapon(4, "molot");
+            weapons["knife"] = new Weapon(2, "knife");
+            weapons["vedro"] = new Weapon(3, "vedro");
+            weapons["sword"] = new Weapon(5, "sword");
+            weapons["axe"] = new Weapon(4, "axe");
+            weapons["gun"] = new Weapon(7, "gun");
+            values = weapons.Values.ToList();
+            keys = weapons.Keys.ToList();
+
+            r = new Random();
+
+        }
+
+       
+
+        static public string Random()
+        {
+            return keys[r.Next(keys.Count)];
+        }
+
+        public static Weapon Get(string name)
+        {
+            return weapons[name];
+        }
+    }
+}
